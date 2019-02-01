f2a70580cf2180dce2afa6c5c3312d4fa7b68bea
Daniel Eiserman <daniel.eiserman@protonmail.com>
2019-02-01 22:19:40 +0100

    Made it possible to point out where to put the respawn point when moving a base.

--- Dialogs/BuildBaseDialog.hpp
+++ Dialogs/BuildBaseDialog.hpp
@@ -1,3 +1,14 @@
+class IntBaseDialog_PlaceButton : IntBaseDialog_Button
+{
+	idc = -1;
+	text = "Place respawn point here";
+	x = 1 * GUI_GRID_W + GUI_GRID_X;
+	y = 3 * GUI_GRID_H + GUI_GRID_Y;
+	w = 10 * GUI_GRID_W;
+	h = 1.8 * GUI_GRID_H;
+	action = " closeDialog 0; ";
+};
+
 class IntBuildBaseDialog
 {
     idd = -1;
@@ -5,19 +16,40 @@ class IntBuildBaseDialog
 
 	class Controls
     {
-		class IntProfessionDialog_DialogSurface : IntBaseDialog_DialogSurface {};
+		class IntMoveBaseDialog_DialogSurface : IntBaseDialog_DialogSurface {};
 		
-		class IntProfessionDialog_CancelButton : IntBaseDialog_CancelButton {
-			action = "call Intrusion_Client_ProfessionDialog_OnCancelButtonPressed;";
-		};
-		
-		class IntProfessionDialog_OkButton : IntBaseDialog_OkButton {
-			action = "call Intrusion_Client_ProfessionDialog_OnOkButtonPressed;";
-		};
-		
-		class IntProfessionDialog_Header : IntBaseDialog_Header
+		class IntMoveBaseDialog_Header : IntBaseDialog_Header
 		{
-			text = "Build Base";
+			text = "Move Base";
+			x = 1 * GUI_GRID_W + GUI_GRID_X;
+		};
+		
+		class IntMoveBaseDialog_PlaceRespawnPointButton : IntBaseDialog_PlaceButton {
+			y = 3 * GUI_GRID_H + GUI_GRID_Y;
+			action = "call Intrusion_Client_MoveBaseDialog_OnRespawnButtonPressed;";
+		};
+		
+		class IntMoveBaseDialog_PlaceVehiclesPointButton : IntBaseDialog_PlaceButton {
+			y = 5 * GUI_GRID_H + GUI_GRID_Y;
+			action = "hint ""Vehicles""";
+		};
+		
+		class IntMoveBaseDialog_PlaceWeaponsPointButton : IntBaseDialog_PlaceButton {
+			y = 7 * GUI_GRID_H + GUI_GRID_Y;
+			action = "hint ""Weapons""";
+		};
+		
+		class IntMoveBaseDialog_CancelButton : IntBaseDialog_CancelButton {
+			text = "Cancel";
+			action = "call Intrusion_Client_MoveBaseDialog_OnCancelButtonPressed;";
+			x = 27.5 * GUI_GRID_W + GUI_GRID_X;
+		};
+		
+		class IntMoveBaseDialog_OkButton : IntBaseDialog_OkButton {
+			text = "Move base now";
+			x = 32.5 * GUI_GRID_W + GUI_GRID_X;
+			w = 7 * GUI_GRID_W;
+			action = "call Intrusion_Client_MoveBaseDialog_OnOkButtonPressed;";
 		};
     };
 };
