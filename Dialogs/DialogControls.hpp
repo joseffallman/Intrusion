3ceec197609b450837f03a1e00a7a688a710da10
Josef Fällman <josef.fallman@gmail.com>
2019-03-24 00:54:10 +0100

    New profession dialog working.

--- Dialogs/DialogControls.hpp
+++ Dialogs/DialogControls.hpp
@@ -1,3 +1,30 @@
+class RscObject
+{
+	access = 0; 
+	idc = -1;
+	type = 82;
+	model = "\a3\Ui_f\objects\Compass.p3d";
+	scale = 0.5;
+	shadow = 0;
+	
+	xBack = 0;
+	yBack = 0;
+	zBack = 0;
+	enableZoom = 0;
+	zoomDuration = 0;
+	inBack = 0;
+	direction[] = {1,0,0};
+	up[] = {0,1,0};
+	tooltip = "";
+	tooltipColorShade[] = {0,0,0,1};
+	tooltipColorText[] = {1,1,1,1};
+	tooltipColorBox[] = {1,1,1,1};
+	onMouseMoving = "";
+	onMouseHolding = "";
+	onMouseDown = "";
+     
+};
+
 class IntBaseDialog_DialogSurface : BOX
 {
 	idc = -1;
@@ -82,3 +109,15 @@ class IntBaseDialog_CancelButton : IntBaseDialog_Button
 	h = 2 * GUI_GRID_H;
 	action = " closeDialog 0; ";
 };
+
+class IntBaseDialog_ListBox : RscListBox
+{
+	id = 2200;
+	x = 0.5 * GUI_GRID_W + GUI_GRID_X;
+	y = 5.5 * GUI_GRID_H + GUI_GRID_Y;
+	w = 19 * GUI_GRID_W;
+	h = 19 * GUI_GRID_H;
+	
+	onLBSelChanged = "";
+};
+
--- Dialogs/ProfessionDialog.hpp
+++ Dialogs/ProfessionDialog.hpp
@@ -1,41 +1,78 @@
 class IntProfessionDialog
 {
-    idd = -1;
+    idd = 2999;
     movingenable = false;
+    
+    class Objects 
+    {
+		class RscModel_2230 : RscObject
+		{
+			idc = 2230;
+			scale = 0.5;
+			z = 8.5;
+			//model = "\A3\armor_f_beta\APC_Tracked_01\APC_Tracked_01_rcws_F.p3d";
+			x = 13 * GUI_GRID_W + GUI_GRID_X;
+			y = 6 * GUI_GRID_H + GUI_GRID_Y;
+			w = 27.5 * GUI_GRID_W;
+			h = 10 * GUI_GRID_H;
+		};
+		
+	};
+
 
 	class Controls
     {
-		class IntProfessionDialog_DialogSurface : IntBaseDialog_DialogSurface {};
 		
 		class IntProfessionDialog_CancelButton : IntBaseDialog_CancelButton {
+			x = 28.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 22.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
 			action = "call Intrusion_Client_ProfessionDialog_OnCancelButtonPressed;";
 		};
-		
+
 		class IntProfessionDialog_OkButton : IntBaseDialog_OkButton {
+			x = 36 * GUI_GRID_W + GUI_GRID_X;
+			y = 22.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 3.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+			text = "Select";
 			action = "call Intrusion_Client_ProfessionDialog_OnOkButtonPressed;";
 		};
-		
+
 		class IntProfessionDialog_Header : IntBaseDialog_Header
 		{
-			text = "Profession";
+			text = "Select group's profession.";
 		};
-		
-		class IntProfessionDialog_Label : IntBaseDialog_Label
+
+		class IntProfessionDialog_ListBox : IntBaseDialog_ListBox
 		{
-			text = "Select a profession";
-			x = 1.5 * GUI_GRID_W + GUI_GRID_X;
-			y = 4 * GUI_GRID_H + GUI_GRID_Y;
-			w = 26 * GUI_GRID_W;
-			h = 1.5 * GUI_GRID_H;
+			idc = 2200;
+			x = 28 * GUI_GRID_W + GUI_GRID_X;
+			y = 1 * GUI_GRID_H + GUI_GRID_Y;
+			w = 12 * GUI_GRID_W;
+			h = 20 * GUI_GRID_H;
+			
+			onLBSelChanged = "call Intrusion_Client_ProfessionDialog_OnListSelectChanged;";
 		};
-		
-		class IntProfessionDialog_Combo : IntBaseDialog_Combo
+
+		class RscText_2215: RscStructuredText
 		{
-			idc = 2100;
-			x = 1.5 * GUI_GRID_W + GUI_GRID_X;
-			y = 5.5 * GUI_GRID_H + GUI_GRID_Y;
-			w = 19 * GUI_GRID_W;
-			h = 1.5 * GUI_GRID_H;
+			idc = 2215;
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 11 * GUI_GRID_H + GUI_GRID_Y;
+			w = 27.5 * GUI_GRID_W;
+			h = 13 * GUI_GRID_H;
 		};
     };
+    
+    
+    class controlsBackground
+    {
+		class IntProfessionDialog_DialogSurface : IntBaseDialog_DialogSurface
+		{
+			
+		};
+    };
+
 };
